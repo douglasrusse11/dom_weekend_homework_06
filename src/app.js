57ef0543173adbb6403785d443afe47b520d2ec1
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-26 14:11:01 +0000

    adds event listener, appends form data to list

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("script loaded")
+
+    document.querySelector('#add-animal-form').addEventListener('submit', function (event) {
+        event.preventDefault()
+        const newListItem = document.createElement('li')
+        const form = event.target
+        newListItem.textContent = `${event.target.commonName.value} - ${event.target.scientificName.value} - ${event.target.conservationStatus.value}`
+        const list = document.getElementById('endangered-animals-list')
+        list.appendChild(newListItem)
+        this.reset()
+    })
 })
