64c546c88b74241c950fa7a707809464f3e21644
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-27 11:37:01 +0000

    adds sort

--- src/app.js
+++ src/app.js
@@ -19,9 +19,14 @@ const createListItem = function (form) {
 
 const addListItem = function (list, listItem) {
     if (list.innerHTML === '') {
-        createDeleteButton()
+        addListFeatures()
     }
     list.appendChild(listItem)
+}
+
+const addListFeatures = function () {
+    createDeleteButton()
+    createSort()
 }
 
 const createDeleteButton = () => {
@@ -38,4 +43,74 @@ const handleDeleteAll = () => {
             list.removeChild(list.firstChild)
         }
         document.querySelector('body').removeChild(event.target)
+        document.querySelector('body').removeChild(document.getElementById('list-sort'))
+        document.querySelector('body').removeChild(document.querySelector('hr'))
+}
+
+const createSort = function () {
+    const sort = document.createElement('select')
+    const options = {
+        '': "Sort by:",
+        'commonNameAsc': "Common Name [A-Z]",
+        'commonNameDesc': "Common Name [Z-A]",
+        'scientificNameAsc': "Scientific Name [A-Z]",
+        'scientificNameDesc': "Scientific Name [Z-A]",
+        'conservationStatusAsc': "Conservation Status [A-Z]",
+        'conservationStatusDesc': "Conservation Status [Z-A]"
+    }
+    for (key in options) {
+        const newOption = document.createElement('option')
+        newOption.value = key
+        newOption.textContent = options[key]
+        if (newOption.value == '') {
+            newOption.disabled = true
+            newOption.selected = true
+        }
+        sort.appendChild(newOption)
+    }
+    sort.id = "list-sort"
+    document.querySelector('h1').after(sort)
+    sort.after(document.createElement('hr'))
+    sort.addEventListener('change', function () {
+        const list = document.querySelector('#endangered-animals-list')
+        const listItems = Array.from(list.childNodes)
+        console.log('sorting')
+        switch (event.target.value) {
+            case 'commonNameAsc':
+                listItems.sort(sortFunction(0))
+                break;
+            case 'commonNameDesc':
+                listItems.reverse(sortFunction(0))
+                break;
+            case 'scientificNameAsc':
+                listItems.sort(sortFunction(1))
+                break;
+            case 'scientificNameDesc':
+                listItems.reverse(sortFunction(1))
+                break;
+            case 'conservationStatusAsc':
+                listItems.sort(sortFunction(2))
+                break;
+            case 'conservationStatusDesc':
+                listItems.reverse(sortFunction(2))
+                break;
+            }
+        list.textContent = ''
+        listItems.forEach(item => list.appendChild(item))
+        })
+
+}
+
+const sortFunction = function(index) {
+    return (item1, item2) => {
+        const a = item1.innerHTML.split('-')[index].trim().toLowerCase()
+        const b = item2.innerHTML.split('-')[index].trim().toLowerCase()
+        if (a < b) {
+            return -1
+        } else if (a == b) {
+            return 0
+        } else {
+            return 1
+        }
+    }
 }
